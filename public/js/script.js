15f3c83d4813e04a5f4ef4fec40d80dafbd8a5b2
James Yuri <137725621+palhelpservice@users.noreply.github.com>
2024-05-04 23:02:39 +0300

    login authenticate fix

--- public/js/script.js
+++ public/js/script.js
@@ -2,11 +2,19 @@ document.addEventListener('DOMContentLoaded', function() {
     const myProjectsButton = document.getElementById('myprojectsbtn');
     const chatRoomButton = document.getElementById('chatroombtn');
     const allProjectsButton = document.getElementById('allprojectsbtn');
+    const logOutButton = document.getElementsByClassName('logout-btn')[0]; // Select the first element with the class
 
     if (myProjectsButton) {
         myProjectsButton.addEventListener('click', function(event) {
             event.preventDefault();
             window.location.href = '/myprojects';
+        });
+    }
+
+    if (logOutButton) {
+        logOutButton.addEventListener('click', function(event) { // Use logOutButton here
+            event.preventDefault();
+            window.location.href = '/';
         });
     }
 
