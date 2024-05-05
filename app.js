64961db0e2c6b6c564425edec50cc8296dfaa258
James Yuri <yuridevs.ke@gmail.com>
2024-05-06 00:16:40 +0300

    chat almost working

--- app.js
+++ app.js
@@ -1,12 +1,45 @@
 const express = require('express');
 const http = require('http');
 const path = require('path');
-const dotenv = require('dotenv');
 const mysql = require('mysql');
 const bodyParser = require('body-parser');
 const session = require('express-session');
 
-dotenv.config();
+
+
+const app = express()
+const PORT = process.env.PORT || 4000
+const server = app.listen(PORT, () => console.log(`💬 server on port ${PORT}`))
+
+const io = require('socket.io')(server)
+
+// Serve static files
+app.use(express.static(path.join(__dirname, 'public')))
+
+let socketsConected = new Set()
+
+io.on('connection', onConnected)
+
+function onConnected(socket) {
+  console.log('Socket connected', socket.id)
+  socketsConected.add(socket.id)
+  io.emit('clients-total', socketsConected.size)
+
+  socket.on('disconnect', () => {
+    console.log('Socket disconnected', socket.id)
+    socketsConected.delete(socket.id)
+    io.emit('clients-total', socketsConected.size)
+  })
+
+  socket.on('message', (data) => {
+    // console.log(data)
+    socket.broadcast.emit('chat-message', data)
+  })
+
+  socket.on('feedback', (data) => {
+    socket.broadcast.emit('feedback', data)
+  })
+}
 
 const connection = mysql.createConnection({
     host: 'localhost',
@@ -23,8 +56,7 @@ connection.connect((err) => {
     console.log('Connected to MySQL database');
 });
 
-const app = express();
-const server = http.createServer(app);
+
 
 const port = process.env.PORT || 3000;
 
@@ -130,11 +162,6 @@ app.get('/get_all_projects', (req, res) => {
 });
 
 
-// Serve static files
-app.use(express.static(path.join(__dirname, 'public')));
 
-// Start server
-server.listen(port, () => {
-    console.log(`Server running on port ${port}`);
-});
+
 
